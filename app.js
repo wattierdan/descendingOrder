7c004a7dd1a03e315b65201ea84b736c0ffb567c
Dan <wattierdan@live.com>
2021-01-03 14:15:50 -0800

    refactored function, way better

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 function descendingOrder(n){
-    let num2string = n.toString()
-    let output = []
-    let output2 = "" 
-    
-    for (let i = 0; i < num2string.length; i ++) {
-      output.push(parseInt(+num2string.charAt(i)))
-    }
-    
-    output.sort((a, b) => b - a)
-    
-    for (let i = 0; i < output.length; i++) {
-            output2 += output[i].toString()
-        
-    }
-
-    return parseInt(output2)
+    return parseInt(String(n).split('').sort().reverse().join(''))
   }
 
   descendingOrder(123456789)
